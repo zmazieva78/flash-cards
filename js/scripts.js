f7375090cf943eb5aeb19c91142369924eaee77d
Zarina Mazieva <zmazieva78@gmail.com>
2019-10-23 12:56:32 -0700

    card changes to back side with two clicks but doesn't reverse back

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $(".card").click(function() {
+    if
+    ($(".front").hasClass("flip")) {
+      $(".front").hide();
+      $(".back").show();
+    }
+      else {
+        $(".front").addClass("flip");
+      }
+  });
+
+
+
+
+
+
+});
